76bbf3d664fc42a022b11657f91b0491b9551210
f-aljardan <fayaljardan@gmail.com>
2023-03-27 22:38:15 +0300

    enter name and print it

--- app/src/main/java/com/example/eval2try/secondActivity2.java
+++ app/src/main/java/com/example/eval2try/secondActivity2.java
@@ -9,7 +9,18 @@ import android.widget.TextView;
 public class secondActivity2 extends AppCompatActivity {
 
 
+    TextView tv ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second2);
 
+        tv = findViewById(R.id.textView);
+        Intent i = getIntent();
+        String str = ((Intent)i).getStringExtra("message_key" );
+        tv.setText("hello"+str+"!");
+
+    }
 
 
 }
